b586e3d1948e64601720ff762de5d1125f53235b
DUDE <nisbis194@gmail.com>
2025-01-05 21:37:39 +0545

    Finished initial setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Digitalclock from "./digitalclock.tsx";
+
+export default function App() {
+  return (
+    <div className="h-screen bg-cover bg-center bg-[url('/public/back.jpg')]">
+      <Digitalclock />
+    </div>
+  );
+}
---
+++ src/digitalclock.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+
+export default function DigitalClock() {
+  const [time, setTime] = useState<Date>(new Date());
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, []);
+
+  // for the title
+  useEffect(() => {
+    document.title = time.toLocaleTimeString();
+  }, [time]);
+
+  function formatTime(time: Date): JSX.Element {
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+    const second = time.getSeconds();
+    let Meridian: string = hours < 12 ? "AM" : "PM";
+
+    return (
+      <div>
+        <span>
+          {hours}.{minutes}.{second} {Meridian}
+        </span>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1 className="flex items-end justify-center text-white font-extrabold text-5xl">
+        Nischal Sharma
+      </h1>
+      <div className="flex items-center  justify-center h-screen text-white font-extrabold  text-8xl">
+        {formatTime(time)}
+      </div>
+    </div>
+  );
+}
